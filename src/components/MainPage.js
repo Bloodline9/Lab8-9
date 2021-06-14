da44108277971dd85e311f0d669874c2f2f87af8
Bloodline9 <70684438+Bloodline9@users.noreply.github.com>
2021-06-15 01:37:24 +0300

    Create MainPage.js

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+import Layout from "./Layout";
+
+const MainPage = () => (
+  <div>
+    <Layout />
+    <div>Это главная страница</div>
+  </div>
+);
+
+export default MainPage;
